01d58d6948ccefdf043451a8b379ebcbcf52ea32
Samiul <93048025+8amiul@users.noreply.github.com>
2025-01-26 18:59:15 +0600

    Add files via upload

--- static/js/music_src.js
+++ static/js/music_src.js
@@ -1,4 +1,353 @@
-const music_src = [
-    "http://192.168.0.106:5500/songs/Blossoms.mp3",
-    "http://192.168.0.106:5500/songs/Advocate.mp3"
-];
+let music_src = 
+[
+  {
+    "link": "http://192.168.0.106:5500/songs/Are You Bored Yet_ (feat. Clairo).mp3",
+    "title": "Are You Bored Yet? (feat. Clairo)",
+    "artist": "Wallows/Clairo",
+    "album": "Nothing Happens",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Blossoms.mp3",
+    "title": "Blossoms",
+    "artist": "Meltt",
+    "album": "Eternal Embers",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Can I Call You Tonight_.mp3",
+    "title": "Can I Call You Tonight?",
+    "artist": "Dayglow",
+    "album": "Fuzzybrain",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Certainty.mp3",
+    "title": "Certainty",
+    "artist": "Temples",
+    "album": "Certainty",
+    "year": "2016"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Dancing with my phone.mp3",
+    "title": "Dancing with my phone",
+    "artist": "HYBS",
+    "album": "Making Steak",
+    "year": "2022"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Down the Line.mp3",
+    "title": "Down the Line",
+    "artist": "Beach Fossils",
+    "album": "Somersault",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Each Time You Fall in Love.mp3",
+    "title": "Each Time You Fall in Love",
+    "artist": "Cigarettes After Sex",
+    "album": "Cigarettes After Sex",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Every Time Around.mp3",
+    "title": "Every Time Around",
+    "artist": "Vansire",
+    "album": "After Fillmore County",
+    "year": "2020"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Flash.mp3",
+    "title": "Flash",
+    "artist": "Cigarettes After Sex",
+    "album": "Cigarettes After Sex",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Gamma Rays.mp3",
+    "title": "Gamma Rays",
+    "artist": "Temples",
+    "album": "Exotico",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Go Higher.mp3",
+    "title": "Go Higher",
+    "artist": "HYBS",
+    "album": "Making Steak",
+    "year": "2022"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Hot Motion.mp3",
+    "title": "Hot Motion",
+    "artist": "Temples",
+    "album": "Hot Motion",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Hot Rod.mp3",
+    "title": "Hot Rod",
+    "artist": "Dayglow",
+    "album": "Fuzzybrain",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/If I Could Find You (Eternity).mp3",
+    "title": "If I Could Find You (Eternity)",
+    "artist": "The Holydrug Couple",
+    "album": "Moonlust",
+    "year": "2015"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/In Between.mp3",
+    "title": "In Between",
+    "artist": "WILLIS",
+    "album": "Locals 4",
+    "year": "2022"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Italo Disco.mp3",
+    "title": "Italo Disco",
+    "artist": "Last Dinosaurs",
+    "album": "Yumeno Garden",
+    "year": "2018"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/(I Want to Be Your) Mirror.mp3",
+    "title": "(I Want to Be Your) Mirror",
+    "artist": "Temples",
+    "album": "Volcano",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/japan.mp3",
+    "title": "japan",
+    "artist": "Yot Club",
+    "album": "japan",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Lamb's Wool.mp3",
+    "title": "Lamb's Wool",
+    "artist": "Foster The People",
+    "album": "Lamb's Wool",
+    "year": "2020"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Let It Happen.mp3",
+    "title": "Let It Happen",
+    "artist": "Tame Impala",
+    "album": "Currents",
+    "year": "2015"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Love Again.mp3",
+    "title": "Love Again",
+    "artist": "Meltt",
+    "album": "Swim Slowly",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Lovers Rock.mp3",
+    "title": "Lovers Rock",
+    "artist": "TV Girl",
+    "album": "French Exit",
+    "year": "2014"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/May 1st.mp3",
+    "title": "May 1st",
+    "artist": "Beach Fossils",
+    "album": "Somersault",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Metamodernity.mp3",
+    "title": "Metamodernity",
+    "artist": "Vansire",
+    "album": "Metamodernity",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Move with the Season.mp3",
+    "title": "Move with the Season",
+    "artist": "Temples",
+    "album": "Sun Structures (Deluxe Version)",
+    "year": "2014"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Mrs Magic.mp3",
+    "title": "Mrs Magic",
+    "artist": "Strawberry Guy",
+    "album": "Mrs Magic",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/New Gold (feat. Tame Impala and Bootie Brown).mp3",
+    "title": "New Gold (feat. Tame Impala and Bootie Brown)",
+    "artist": "Gorillaz/Tame Impala/Bootie Brown",
+    "album": "Cracker Island",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/No More Lies.mp3",
+    "title": "No More Lies",
+    "artist": "Thundercat/Tame Impala",
+    "album": "No More Lies",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Parking Lots.mp3",
+    "title": "Parking Lots",
+    "artist": "Plums",
+    "album": "Jen",
+    "year": "2015"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Past Life.mp3",
+    "title": "Past Life",
+    "artist": "Tame Impala",
+    "album": "Currents",
+    "year": "2015"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Powerlines.mp3",
+    "title": "Powerlines",
+    "artist": "Tame Impala",
+    "album": "Currents B-Sides & Remixes",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Quarterback.mp3",
+    "title": "Quarterback",
+    "artist": "Wallows",
+    "album": "Quarterback",
+    "year": "2021"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Saturnine.mp3",
+    "title": "Saturnine",
+    "artist": "Mystery Jets",
+    "album": "Curve Of The Earth",
+    "year": "2016"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Shoot.mp3",
+    "title": "Shoot",
+    "artist": "Promoting Sounds/Ron Grams",
+    "album": "Shoot",
+    "year": "2021"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Sky So Blue.mp3",
+    "title": "Sky So Blue",
+    "artist": "Hot Flash Heat Wave",
+    "album": "Sky So Blue",
+    "year": "2019"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Social Jetlag.mp3",
+    "title": "Social Jetlag",
+    "artist": "Beach Fossils",
+    "album": "Somersault",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/So Good at Being in Trouble.mp3",
+    "title": "So Good at Being in Trouble",
+    "artist": "Unknown Mortal Orchestra",
+    "album": "II (10 Year Anniversary Edition)",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Something Stupid (From _Better Call Saul_).mp3",
+    "title": "Something Stupid (From \"Better Call Saul\")",
+    "artist": "Lola Marsh",
+    "album": "Something Stupid (From \"Better Call Saul\")",
+    "year": "2018"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Staring.mp3",
+    "title": "Staring",
+    "artist": "Tipling Rock",
+    "album": "Staring",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Sugar.mp3",
+    "title": "Sugar",
+    "artist": "Beach Fossils",
+    "album": "Somersault",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Tangerine.mp3",
+    "title": "Tangerine",
+    "artist": "Beach Fossils",
+    "album": "Somersault",
+    "year": "2017"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/The Absent Sea.mp3",
+    "title": "The Absent Sea",
+    "artist": "Meltt",
+    "album": "Eternal Embers",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/The Bay.mp3",
+    "title": "The Bay",
+    "artist": "Yot Club",
+    "album": "The Bay",
+    "year": "2020"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/The Fire.mp3",
+    "title": "The Fire",
+    "artist": "Meltt",
+    "album": "Eternal Embers",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/THE SHADE.mp3",
+    "title": "THE SHADE",
+    "artist": "Rex Orange County",
+    "album": "WHO CARES?",
+    "year": "2022"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/What You Know.mp3",
+    "title": "What You Know",
+    "artist": "Two Door Cinema Club",
+    "album": "Tourist History",
+    "year": "2010"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Wings Of Time - From the Motion Picture Dungeons & Dragons_ Honor Among Thieves.mp3",
+    "title": "Wings Of Time - From the Motion Picture Dungeons & Dragons: Honor Among Thieves",
+    "artist": "Tame Impala",
+    "album": "Wings Of Time (From the Motion Picture Dungeons & Dragons: Honor Among Thieves)",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Within You, Within Me.mp3",
+    "title": "Within You, Within Me",
+    "artist": "Meltt",
+    "album": "Eternal Embers",
+    "year": "2023"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/You Are the Right One.mp3",
+    "title": "You Are the Right One",
+    "artist": "Sports",
+    "album": "Naked All the Time",
+    "year": "2015"
+  },
+  {
+    "link": "http://192.168.0.106:5500/songs/Young Folks.mp3",
+    "title": "Young Folks",
+    "artist": "Peter Bjorn and John",
+    "album": "Writer's Block",
+    "year": "2006"
+  }
+]
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,95 @@
 const totalMusic = music_src.length;
 let currentMusicPlaying = 0;
-var audio = new Audio(music_src[currentMusicPlaying]);
+//var audio = new Audio(music_src[currentMusicPlaying].src);
+var audio = new Audio();
 
-audio.volume = (20 / 100);
+audio.volume = 0.5;
 
 let timeMusic = document.querySelector('.musicTime');
 let titleMusic = document.querySelector('.musicTitle');
 let artistMusic = document.querySelector('.musicArtist');
 let AlbumMusic = document.querySelector('.musicAlbum');
 
+
+
+let musicVolumeSlider = document.querySelector('#volume-slider')
+musicVolumeSlider.value = audio.volume * 100;
+let musicVolumeHTML = document.querySelector('.musicVolume');
+
+let playPauseButtonIcon = document.querySelector(".play-pause-svg"); 
+
+musicVolumeSlider.addEventListener("change", function () {
+    audio.volume = musicVolumeSlider.value / 100;
+})
+
+function musicLoad(musicPlayingNumber) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio = null;
+    currentMusicPlaying = musicPlayingNumber;
+    fetchMusicTags();
+    audio = new Audio(music_src[currentMusicPlaying].link)
+    audio.volume = musicVolumeSlider.value / 100;
+    audio.play()
+}
+
 let pauseMusic = document.querySelector('#music-pause');
 pauseMusic.addEventListener("click", function () {
     if (audio.paused)
-        audio.play();
+        if (audio.src == "")
+            musicLoad(currentMusicPlaying)
+        else
+            audio.play();
     else
         audio.pause();
 });
 
+let musicList = document.querySelector('.music-list');
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < totalMusic; i++)
+    {
+        let listItem = document.createElement('li');
+        let listItemButton = document.createElement('button');
+        listItemButton.setAttribute('type', "button");
+        listItemButton.setAttribute('class', "music-list-item");
+
+        listItemButton.innerText = music_src[i].title;
+
+        listItemButton.addEventListener("click", function () {
+            currentMusicPlaying = i;
+            musicLoad(currentMusicPlaying)
+        })
+
+        listItem.appendChild(listItemButton)
+        musicList.appendChild(listItem)
+    }
+
+})
+
+
 
 function fetchMusicTags () {
-    jsmediatags.read(music_src[currentMusicPlaying], {
+    titleMusic.innerText = music_src[currentMusicPlaying].title;
+    artistMusic.innerHTML = music_src[currentMusicPlaying].artist;
+    AlbumMusic.innerText = music_src[currentMusicPlaying].album;
+
+    jsmediatags.read(music_src[currentMusicPlaying].link, {
+        onSuccess: function(tag) {
+            var picture = tag.tags.picture;
+            var base64String = "";
+            for (var i = 0; i < picture.data.length; i++) {
+                base64String += String.fromCharCode(picture.data[i]);
+            }
+            var albumCoverUri = "data:" + picture.format + ";base64," + window.btoa(base64String);
+
+            document.getElementById('musicAlbum-cover').src = albumCoverUri;
+    }});
+}
+fetchMusicTags()
+
+/*
+function fetchMusicTags () {
+    jsmediatags.read(music_src[currentMusicPlaying].link, {
         onSuccess: function(tag) {
             titleMusic.innerText = tag.tags.title;
             artistMusic.innerText = tag.tags.artist;
@@ -39,67 +109,116 @@ function fetchMusicTags () {
     });
 }
 fetchMusicTags();
+*/
+
+
 
 let nextMusic = document.querySelector('#music-next');
-nextMusic.addEventListener("click", function () {
-    if (currentMusicPlaying < totalMusic-1)
-    {
-        currentMusicPlaying++;
-        audio.pause();
-        audio.currentTime = 0;
-        audio = null;
-        fetchMusicTags();
-        audio = new Audio(music_src[currentMusicPlaying]);
-        audio.play();
 
-    }
-}) 
+
+function nextMusicFunc() {
+    if (audio.src == "")
+        musicLoad(currentMusicPlaying)
+    if (currentMusicPlaying < totalMusic-1)
+        {
+            currentMusicPlaying++;
+            musicLoad(currentMusicPlaying)
+        }
+}
+nextMusic.addEventListener("click", nextMusicFunc); 
 
 let previousMusic = document.querySelector('#music-previous');
 previousMusic.addEventListener("click", function () {
     if (currentMusicPlaying > 0)
     {
         currentMusicPlaying--;
-        audio.pause();
-        audio.currentTime = 0;
-        audio = null;
-        fetchMusicTags();
-        audio = new Audio(music_src[currentMusicPlaying]);
-        audio.play();
+        musicLoad(currentMusicPlaying);
     }
 })
 
 let forwardMusic = document.querySelector('#music-forward');
+const forwardSecond = 10;
 forwardMusic.addEventListener("click", function () {
-    if (audio.currentTime + 10 <= audio.duration)
-        audio.currentTime += 10;
+    if (audio.currentTime + forwardSecond <= audio.duration)
+        audio.currentTime += forwardSecond;
 })
 
 let rewindMusic = document.querySelector('#music-rewind');
+const rewindSecond = 10;
 rewindMusic.addEventListener("click", function () {
-    if (audio.currentTime - 10 >= 0)
-        audio.currentTime -= 10;
+    if (audio.currentTime - rewindSecond >= 0)
+        audio.currentTime -= rewindSecond;
+})
+
+let listToggleBtn = document.querySelector(".list-toggle-btn")
+let listToggleBtnSvg = document.querySelector(".list-toggle-btn-svg")
+musicList.style.display = "none"
+listToggleBtn.addEventListener("click", function () {
+    if (musicList.style.display == "none") {
+        musicList.style.display = "block"
+        listToggleBtnSvg.src = "/static/svg/chevron-up.svg"
+    }
+    else {
+        musicList.style.display = "none"
+        listToggleBtnSvg.src = "/static/svg/chevron-down.svg"
+    }
 })
 
 
+function printMusic() {
 
-function printTimeMusic() {
+    if (audio.paused)
+        playPauseButtonIcon.src = "static/svg/play.svg";
+    else 
+        playPauseButtonIcon.src = "static/svg/pause.svg";
 
-    let totalSeconds = Math.trunc(audio.currentTime);
-    let hours = Math.floor(totalSeconds / 3600);
-    let minutes = Math.floor((totalSeconds % 3600) / 60);
-    let seconds = totalSeconds % 60;
+    if (currentMusicPlaying != totalMusic-1)
+    {
+        if (audio.ended)
+            nextMusicFunc();
+    }
 
-    let totalSecondsDuration = Math.trunc(audio.duration);
-    let hoursDur = Math.floor(totalSecondsDuration / 3600);
-    let minutesDur = Math.floor((totalSecondsDuration % 3600) / 60);
-    let secondsDur = totalSecondsDuration % 60;
+    musicVolumeHTML.innerText = `Vol: ${Math.trunc(audio.volume * 100)}%`;
 
-    if (totalSecondsDuration >= 3600)
-        timeMusic.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} / ${hours.toString().padStart(2, '0')}:${minutesDur.toString().padStart(2, '0')}:${secondsDur.toString().padStart(2, '0')}`;
-    else
-        timeMusic.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} / ${minutesDur.toString().padStart(2, '0')}:${secondsDur.toString().padStart(2, '0')}`;
-    setTimeout(printTimeMusic, 100);
+    if (audio.src != "") {
+        
+        let totalSecondsDuration = Math.trunc(audio.duration);
+        let hoursDur = Math.floor(totalSecondsDuration / 3600);
+        let minutesDur = Math.floor((totalSecondsDuration % 3600) / 60);
+        let secondsDur = totalSecondsDuration % 60;
+
+        let totalSeconds = Math.trunc(audio.currentTime);
+        let hours = Math.floor(totalSeconds / 3600);
+        let minutes = Math.floor((totalSeconds % 3600) / 60);
+        let seconds = totalSeconds % 60;
+
+
+        if (totalSecondsDuration >= 3600)
+            timeMusic.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} / ${hours.toString().padStart(2, '0')}:${minutesDur.toString().padStart(2, '0')}:${secondsDur.toString().padStart(2, '0')}`;
+        else
+            timeMusic.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} / ${minutesDur.toString().padStart(2, '0')}:${secondsDur.toString().padStart(2, '0')}`;
+    }
+        setTimeout(printMusic, 100);
 }
-printTimeMusic();
+printMusic();
 
+
+
+/*
+let DATA = [
+	
+]
+for (let i = 0; i < links.length; i++)
+{
+	let obj = new Object();
+	obj.link = links[i];
+	jsmediatags.read(links[i], {
+    onSuccess: function(tag) {
+    obj.title = tag.tags.title;
+		obj.artist = tag.tags.artist;
+		obj.album = tag.tags.album;
+		obj.year = tag.tags.year;		
+  }});
+	DATA[i] = obj;
+}
+*/
